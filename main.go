811868dea83b1366ee9f3e803be4bd4730c52995
kafetheine <kafetheine@gmail.com>
2022-08-16 17:56:18 +0700

    Join the battle

--- main.go
+++ main.go
@@ -79,11 +79,11 @@ func play(input ArenaUpdate) (response string) {
 			}
 		}
 	}
-	// if len(actionList) == 0 {
-	// 	commands := []string{"F", "R", "L", "T"}
-	// 	rand := rand2.Intn(4)
-	// 	actionList = append(actionList, commands[rand])
-	// }
+	if len(actionList) == 0 {
+		commands := []string{"F", "R", "L", "T"}
+		rand := rand2.Intn(4)
+		actionList = append(actionList, commands[rand])
+	}
 	action, actionList = actionList[0], actionList[1:]
 	return action
 }
